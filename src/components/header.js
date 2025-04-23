bea082abee1740ed0883dd88f7fca857a483ba86
cacciabella12 <cacciabella12@gmail.com>
2025-04-23 17:41:58 +0200

    Descrizione delle modifiche

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,9 @@ import './header.css';
 import fotoHeader from '../img/foto_header.jpg';
 import logoSito from '../img/logo_sito.png';
 
-
 function Hero({ scrollToComponent }) {
   const [showNav, setShowNav] = useState(false);
-
+  
   const menuItems = [
     { name: 'Home', path: '/Calendar_Event' },
     { name: 'Chi Sono', path: '/corsi' },
@@ -14,42 +13,40 @@ function Hero({ scrollToComponent }) {
     { name: 'Contatti', path: '/contatti' },
     { name: 'Eventi', path: '/eventi' }
   ];
-
- 
   
-
-
   return (
     <div className="hero" style={{ backgroundImage: `url(${fotoHeader})` }}>
-      <nav className="navbar">
-      <img src={logoSito} style={{ width: '150px', height: 'auto' }} />
+      <div className="navbar-container">
+        <nav className="navbar">
+          <div className="navbar-top">
+            <img src={logoSito} style={{ width: '150px', height: 'auto' }} alt="Logo" />
+            
+            <button
+              className="navbar-toggler"
+              onClick={() => setShowNav(!showNav)}
+              aria-label={showNav ? 'Chiudi menu' : 'Apri menu'}
+            >
+              {showNav ? '✕' : '☰'}
+            </button>
+          </div>
+        </nav>
 
-        <button
-          className="navbar-toggler"
-          onClick={() => setShowNav(!showNav)}
-          aria-label={showNav ? 'Chiudi menu' : 'Apri menu'}
-        >
-          {showNav ? '✕' : '☰'}
-        </button>
-      </nav>
-
-      <div className={`menu-dropdown ${showNav ? 'open' : ''}`}>
-        <ul className="navbar-nav"
-       >
-          {menuItems.map((item, index) => (
-            <li key={index} className="navbar-item">
-              <a href={item.path} className="navbar-link" onClick={() => setShowNav(false)}>
-                {item.name}
-              </a>
-            </li>
-          ))}
-        </ul>
+        <div className={`menu-dropdown ${showNav ? 'open' : ''}`}>
+          <ul className="navbar-nav">
+            {menuItems.map((item, index) => (
+              <li key={index} className="navbar-item">
+                <a href={item.path} className="navbar-link" onClick={() => setShowNav(false)}>
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <div className="hero-mask">
         <div className="hero-content">
-        
-           <h1>CORSI MAXIENDURO</h1> 
+          <h1>CORSI MAXIENDURO</h1>
           <button className="hero-btn" onClick={scrollToComponent}>
             PER SAPERNE DI PIÚ
           </button>
